f857043aad1722e8807ba167bfbdface09e3c1a9
Jason Smiles <jason@1610.io>
2025-02-05 16:55:06 +0100

    Initial commit

---
+++ src/routes/logo.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	let { ...props } = $props();
+</script>
+
+<svg
+	width="1em"
+	height="1em"
+	viewBox="0 0 50 50"
+	fill="none"
+	xmlns="http://www.w3.org/2000/svg"
+	{...props}
+>
+	<g clip-path="url(#clip0_1_2)">
+		<rect width="25" height="25" fill="#3385FF" />
+		<rect x="25" width="25" height="25" rx="12.5" fill="#00FF67" />
+		<rect y="25" width="25" height="25" fill="#FF5733" />
+		<rect x="25" y="25" width="25" height="25" fill="#FFD83B" />
+	</g>
+	<defs>
+		<clipPath id="clip0_1_2">
+			<rect width="50" height="50" fill="white" />
+		</clipPath>
+	</defs>
+</svg>
