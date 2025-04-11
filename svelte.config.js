3b3c57c080070103bc7339251f899d4d5583f51c
Jason Smiles <jason@1610.io>
2025-04-11 15:24:03 +0200

    Update svelte adapter for pages

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-cloudflare';
+import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 const dev = process.argv.includes('dev');
@@ -13,7 +13,12 @@ const config = {
         // adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
         // If your environment is not supported, or you settled on a specific environment, switch out the adapter.
         // See https://svelte.dev/docs/kit/adapters for more information about adapters.
-        adapter: adapter(),
+        adapter: adapter({
+            pages: 'build',
+            assets: 'build',
+            fallback: null,
+            precompress: false
+        }),
         paths: {
             base: dev ? '' : process.env.BASE_PATH
         }
